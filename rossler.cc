9ae3c337a6ea63d66d379d81cbf29336e675df84
Eoin O Connor <oconne19@hpc07.tchpc.tcd.ie>
2018-09-24 11:51:33 +0100

    start of problem 2 (not working)

---
+++ rossler.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using std::cout;
+
+
+int main(int argc, char const *argv[]) {
+  std::vector<double> x(3);
+  x[0]=0;
+  x[1]=0;
+  x[2]=0;
+  cout << x << '\n';
+  return 0;
+
+  
+
+
+
+
+
+
+}
